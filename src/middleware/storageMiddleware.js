426ede9c145331eb28a57669432f19529abfbe12
Brandon Kobel <brandon.kobel@gmail.com>
2016-07-20 21:11:07 -0400

    Refactoring to use a DownloadTask
    The previous return value was rather confusing, so I'm hoping this
    approach is more clear to future readers

--- src/middleware/storageMiddleware.js
+++ src/middleware/storageMiddleware.js
@@ -1,10 +1,12 @@
+'use strict';
+
 module.exports = function (req, res, next) {
   const aws = require('aws-sdk');
 
-  var accessKeyId = req.webtaskContext.secrets.awsAccessKeyId;
-  var bucket = req.webtaskContext.secrets.awsBucket;
-  var secretAccessKey = req.webtaskContext.secrets.awsSecretAccessKey;
-  var region = req.webtaskContext.secrets.awsRegion;
+  const accessKeyId = req.webtaskContext.secrets.awsAccessKeyId;
+  const bucket = req.webtaskContext.secrets.awsBucket;
+  const secretAccessKey = req.webtaskContext.secrets.awsSecretAccessKey;
+  const region = req.webtaskContext.secrets.awsRegion;
 
   aws.config.update({
     accessKeyId: accessKeyId,
@@ -14,41 +16,45 @@ module.exports = function (req, res, next) {
 
   req.storage = {
     upload: function (key, stream, done) {
-      var s3 = new aws.S3({params: {Bucket: bucket, Key: key}});
+      const s3 = new aws.S3({params: {Bucket: bucket, Key: key}});
       s3.upload({Body: stream})
         .send(function(err) {
           done(err);
       });
     },
     download: function (key) {
-      const params = { Bucket: bucket, Key: key };
-      var s3 = new aws.S3();
+      const DownloadTask = function (key) {
+        const s3 = new aws.S3();
 
-      // all the nonsense below is so we can receive error events and if we don't subscribe
-      // to them everywhere below, aws-sdk is throwing exceptions that crash the app :/
-      var errorCallback = function () {};
+        // The aws-sdk takes a non-traditional approach to error handling, so we're
+        // doing out best to hide the nuances. If we don't subscribe to the error
+        // event where we are below, it throws an error crashing the app if something
+        // goes wrong.
+        let errorCallback = function () {};
 
-      var request = s3.getObject(params).on('error', errorCallback);
+        const params = { Bucket: bucket, Key: key };
+        const request = s3.getObject(params).on('error', errorCallback);
 
-      var result =  {
-        on: function (evt, cb) {
-          // we're only propagating the 'error' event at this time
-          switch (evt) {
-            case 'error':
-              errorCallback = cb;
-              break;
-            default:
-              throw new Error('Unsupported event type');
+        return {
+          on: function (evt, cb) {
+            // we're only propagating the 'error' event at this time
+            switch (evt) {
+              case 'error':
+                errorCallback = cb;
+                break;
+              default:
+                throw new Error('Unsupported event type');
+            }
+            return this;
+          },
+          pipe: function (stream) {
+            request.createReadStream().on('error', errorCallback).pipe(stream);
+            return this;
           }
-          return result;
-        },
-        pipe: function (stream) {
-          request.createReadStream().on('error', errorCallback).pipe(stream);
-          return result;
-        }
+        };
       };
 
-      return result;
+      return new DownloadTask(key);
     }
   };
 

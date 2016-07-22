d392de16ddc9cbe3eb0f46c528dc342c92f56e54
Brandon Kobel <brandon.kobel@gmail.com>
2016-07-22 09:58:47 -0400

    Experimenting with streaming the file to the client, relies on a single instance, not horizontally scalable without a properly configured load-balancer

--- src/api.js
+++ src/api.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const logo = require('./logo');
+const PassThrough = require('stream').PassThrough;
 const storageMiddleware = require('./middleware/storageMiddleware');
 const url = require('url');
 const urlJoin = require('url-join');
@@ -16,58 +17,62 @@ function getScheme(req) {
   return req.secure ? 'https' : 'http';
 }
 
-function getUrl(req) {
+function getUrl(req, key) {
   const host = req.headers.host;
   const requestUrl = url.parse(req.originalUrl);
   const path = requestUrl.pathname;
   const scheme = getScheme(req);
-  return scheme + '://' + host + path;
+  const baseUrl = scheme + '://' + host + path;
+  if (!key) {
+    return baseUrl;
+  }
+
+  return urlJoin(baseUrl, key);
 }
 
-api.use(storageMiddleware);
+const streams = {};
+
 api.use(writeLineMiddleware);
 
 api.get('/', function (req, res) {
+  const key = uuid.v4();
+
   res.writeLine(logo);
   res.writeLine();
-  res.writeLine('Example using cURL:');
-  res.writeLine('curl -X POST ' + getUrl(req) + ' --data-binary @filename.extension');
+  res.writeLine('First, initiate the receiver: ');
+  res.writeLine('curl ' + getUrl(req, key) + ' -o destination.extension');
+  res.writeLine();
+  res.writeLine('Then, post the file: ');
+  res.writeLine('curl -X POST ' + getUrl(req, key) + ' --data-binary @source.extension');
   res.writeLine();
   res.end();
 });
 
-api.post('/', function (req, res, next) {
-  const key = uuid.v4();
+api.get('/:key', function (req, res) {
+  const key = req.params.key;
+
+  const stream = streams[key] = new PassThrough();
+  res.set('Content-Type', 'application/octet-stream');
+  stream.pipe(res);
+});
+
+api.post('/:key', function (req, res) {
+  const key = req.params.key;
 
   if (parseInt(req.headers['content-length']) === 0) {
     res.status(400).writeLine('This isn\'t a non-empty file').end();
     return;
   }
 
-  req.storage.upload(key, req, function (err) {
-    if (err) {
-      return next(err);
-    }
+  const stream = streams[key];
+  if (!stream) {
+    res.status(404).end();
+  }
 
-    res.writeLine();
-    res.writeLine('Here\'s your file:');
-    res.writeLine(urlJoin(getUrl(req), key));
-    res.writeLine();
+  req.pipe(stream).on('end', function () {
     res.end();
+    delete streams[key];
   });
 });
-
-api.get('/:key', function (req, res, next) {
-  const key = req.params.key;
-
-  res.set('Content-Type', 'application/octet-stream');
-  req.storage.download(key)
-    .on('error', function (err) {
-      next(err);
-    })
-    .pipe(res);
-});
-
-
 
 module.exports = api;
